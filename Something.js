444d13f223f5197d4c5d5573229366bd9a46afa1
Rryo10 <sophia@legacyguam.com>
2023-01-26 22:52:34 -0500

    Based

---
+++ Something.js
@@ -0,0 +1,21 @@
+function Wrong() {
+    alert("Try again...")
+};
+
+function Right() {
+        let x = document.getElementById("yes");
+        if (x.style.display == "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "block";
+        }
+      
+};
+
+function Show() 
+{
+    let image = document.getElementById("sike");
+            image.src = "https://media.tenor.com/jDii9nLI-14AAAAC/arcaea-cry.gif"
+            document.getElementById("btnID")
+                    .style.display = "none";
+}
